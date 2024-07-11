1a39a1160128aaabd65b1b99f9688f9f22ed8c70
UrfinDeuce <urfin1995@gmail.com>
2024-07-11 13:19:17 +0300

    add: more config

--- src/views/shared/token-balance/token-balance.view.tsx
+++ src/views/shared/token-balance/token-balance.view.tsx
@@ -28,7 +28,7 @@ export const TokenBalance: FC<TokenBalanceProps> = ({ chainId, spinnerSize, toke
 
 
   if (chainId !== "ethereum" && token.symbol === "ETH") {
-    symbol = "ETH";
+    symbol = "MORE";
   }
 
   if (!token.balance) {
